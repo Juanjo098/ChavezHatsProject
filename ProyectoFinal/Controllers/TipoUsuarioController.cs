1a160bdcfacdeb70fd3d1c6c216a6178b7b4228d
Juanjo098 <89929790+Juanjo098@users.noreply.github.com>
2023-05-17 16:31:15 -0600

    Tipos de usuarios

---
+++ ProyectoFinal/Controllers/TipoUsuarioController.cs
@@ -0,0 +1,153 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoFinal.Clases.Consultas;
+using ProyectoFinal.Clases.Editar;
+using ProyectoFinal.Clases.Insertar;
+using ProyectoFinal.Models;
+
+namespace ProyectoFinal.Controllers
+{
+    public class TipoUsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            try
+            {
+                using (CHAVEZ_HATSContext db = new CHAVEZ_HATSContext())
+                {
+                    List<SimpleCONS> lista = new List<SimpleCONS>();
+                    lista = (
+                            from item in db.TiposUsarios
+                            where item.Hab == true
+                            select new SimpleCONS { id = item.IdTipoUsuario, nombre = item.NomTipoUsuario }
+                        ).ToList();
+                    return View(lista);
+                }
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        public IActionResult Insertar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Insertar(SimpleINST insert)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(insert);
+            }
+            try
+            {
+                using (CHAVEZ_HATSContext db = new CHAVEZ_HATSContext())
+                {
+                    TiposUsario item = new TiposUsario { NomTipoUsuario = insert.nombre };
+                    db.TiposUsarios.Add(item);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Detalles(int id)
+        {
+            try
+            {
+                using (CHAVEZ_HATSContext db = new CHAVEZ_HATSContext())
+                {
+                    TiposUsario item = db.TiposUsarios.Find(id);
+                    if (item != null)
+                    {
+                        SimpleCONS cons = new SimpleCONS {id = item.IdTipoUsuario, nombre = item.NomTipoUsuario };
+                        return View(cons);
+                    }
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int id)
+        {
+            try
+            {
+                using (CHAVEZ_HATSContext db = new CHAVEZ_HATSContext())
+                {
+                    TiposUsario item = db.TiposUsarios.Find(id);
+                    if (item != null)
+                    {
+                        SimpleEDIT cons = new SimpleEDIT { id = item.IdTipoUsuario, nombre = item.NomTipoUsuario };
+                        return View(cons);
+                    }
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Editar(SimpleEDIT edit)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return RedirectToAction("Editar", new { id = edit.id });
+                }
+                using (CHAVEZ_HATSContext db = new CHAVEZ_HATSContext())
+                {
+                    TiposUsario item = db.TiposUsarios.Find(edit.id);
+                    if (item != null)
+                    {
+                        item.NomTipoUsuario = edit.nombre;
+                        db.SaveChanges();
+                        return RedirectToAction("Index");
+                    }
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Eliminar(int id)
+        {
+            try
+            {
+                using (CHAVEZ_HATSContext db = new CHAVEZ_HATSContext())
+                {
+                    TiposUsario item = db.TiposUsarios.Find(id);
+                    if (item != null)
+                    {
+                        item.Hab = false;
+                        db.SaveChanges();
+                    }
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
