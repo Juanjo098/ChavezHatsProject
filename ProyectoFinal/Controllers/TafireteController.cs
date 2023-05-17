0ec862c33e1a86db86c136d79b08e75cc4ae24f2
Juanjo098 <89929790+Juanjo098@users.noreply.github.com>
2023-05-17 11:08:30 -0600

    Tafirete

---
+++ ProyectoFinal/Controllers/TafireteController.cs
@@ -0,0 +1,153 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoFinal.Clases.Consultas;
+using ProyectoFinal.Clases.Editar;
+using ProyectoFinal.Clases.Insertar;
+using ProyectoFinal.Models;
+
+namespace ProyectoFinal.Controllers
+{
+    public class TafireteController : Controller
+    {
+        public IActionResult Index()
+        {
+            try
+            {
+                using (CHAVEZ_HATSContext db = new CHAVEZ_HATSContext())
+                {
+                    List<SimpleCONS> lista = new List<SimpleCONS>();
+                    lista = (
+                            from item in db.Tafiletes
+                            where item.Hab == true
+                            select new SimpleCONS { id = item.IdTafiletes, nombre = item.NomTafiletes }
+                        ).ToList();
+                    return View(lista);
+                }
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        public IActionResult Insertar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Insertar(SimpleINST insert)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(insert);
+            }
+            try
+            {
+                using (CHAVEZ_HATSContext db = new CHAVEZ_HATSContext())
+                {
+                    Tafilete item = new Tafilete { NomTafiletes = insert.nombre };
+                    db.Tafiletes.Add(item);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Detalles(int id)
+        {
+            try
+            {
+                using (CHAVEZ_HATSContext db = new CHAVEZ_HATSContext())
+                {
+                    Tafilete item = db.Tafiletes.Find(id);
+                    if (item != null)
+                    {
+                        SimpleCONS cons = new SimpleCONS { id = item.IdTafiletes, nombre = item.NomTafiletes };
+                        return View(cons);
+                    }
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int id)
+        {
+            try
+            {
+                using (CHAVEZ_HATSContext db = new CHAVEZ_HATSContext())
+                {
+                    Tafilete item = db.Tafiletes.Find(id);
+                    if (item != null)
+                    {
+                        SimpleEDIT cons = new SimpleEDIT { id = item.IdTafiletes, nombre = item.NomTafiletes };
+                        return View(cons);
+                    }
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Editar(SimpleEDIT edit)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return RedirectToAction("Editar", new { id = edit.id });
+                }
+                using (CHAVEZ_HATSContext db = new CHAVEZ_HATSContext())
+                {
+                    Tafilete item = db.Tafiletes.Find(edit.id);
+                    if (item != null)
+                    {
+                        item.NomTafiletes = edit.nombre;
+                        db.SaveChanges();
+                        return RedirectToAction("Index");
+                    }
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Eliminar(int id)
+        {
+            try
+            {
+                using (CHAVEZ_HATSContext db = new CHAVEZ_HATSContext())
+                {
+                    Tafilete item = db.Tafiletes.Find(id);
+                    if (item != null)
+                    {
+                        item.Hab = false;
+                        db.SaveChanges();
+                    }
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
