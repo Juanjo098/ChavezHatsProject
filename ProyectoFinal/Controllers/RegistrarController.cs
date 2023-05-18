3e31363dd400f05bef2188fd5537728305540655
Juanjo098 <89929790+Juanjo098@users.noreply.github.com>
2023-05-18 16:26:30 -0600

    Registrarse

---
+++ ProyectoFinal/Controllers/RegistrarController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoFinal.Clases.Insertar;
+using ProyectoFinal.Models;
+
+namespace ProyectoFinal.Controllers
+{
+    public class RegistrarController : Controller
+    {
+        private static int ID_CLIENTE = 2;
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(UsuarioINST item)
+        {
+            if (!ModelState.IsValid) return View(item);
+
+            try
+            {
+                using (CHAVEZ_HATSContext db = new CHAVEZ_HATSContext())
+                {
+                    if (db.Usuarios.FirstOrDefault(e => e.Correo == item.correo) != null) {
+                        ViewBag.error = "Ese correo ya se encuentra registrado, intente con otro.";
+                        return View("Index");
+                    } 
+                    Usuario u = new Usuario
+                    {
+                        NomUsuario = item.nombre,
+                        Correo = item.correo.ToLower(),
+                        Contrasena = item.contrasena,
+                        IdTipoUsuario= ID_CLIENTE
+                    };
+
+                }
+                return RedirectToAction("Index", "Home");
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+    }
+}
