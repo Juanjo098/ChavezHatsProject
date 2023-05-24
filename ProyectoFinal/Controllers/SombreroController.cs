b1ff7fce85a6dddb16f263f4d90ac78bd41700c4
Juanjo098 <89929790+Juanjo098@users.noreply.github.com>
2023-05-24 14:34:24 -0600

    Creado index y detalles de sombreros

---
+++ ProyectoFinal/Controllers/SombreroController.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoFinal.Clases.Consultas;
+using ProyectoFinal.Models;
+
+namespace ProyectoFinal.Controllers
+{
+    public class SombreroController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<SombreroCONS> lista = GetListaSombreros();
+
+            if (lista == null) return RedirectToAction("Error", "Home");
+
+            return View(lista);
+        }
+
+        [HttpPost]
+        public IActionResult Detalles(int id)
+        {
+            SombreroCONSD sombrero = GetSombreroDetallado(id);
+
+            if (sombrero == null) return RedirectToAction("Error", "Home");
+
+            return View(sombrero);
+        }
+
+        private List<SombreroCONS> GetListaSombreros()
+        {
+            try
+            {
+                using (CHAVEZ_HATSContext db = new CHAVEZ_HATSContext())
+                {
+                    List<SombreroCONS> lista = (
+                            from item in db.Sombreros
+                            join talla in db.Tallas
+                            on item.IdTalla equals talla.IdTalla
+                            join tam in db.TamanoTallas
+                            on item.IdTalla equals tam.IdTalla
+                            join modelo in db.Modelos
+                            on item.IdModelo equals modelo.IdModelo
+                            join clase in db.Clases
+                            on item.IdClase equals clase.IdClase
+                            where item.Hab == true
+                            select new SombreroCONS {
+                                id = item.IdSombrero,
+                                idMaterial = item.IdMaterial,
+                                nomModelo = modelo.NomModelo,
+                                nomClase = clase.NomClase,
+                                nomTalla = talla.NomTalla,
+                                tamTalla = tam.TamTalla,
+                                medFalda = (float)item.MedFalda,
+                                precio = (float)item.Precio,
+                                stock = (int)item.Stock,
+                                personalizado = (bool)item.Personalizado? "Sí" : "No",
+                                idModelo = item.IdModelo,
+                                idClase = item.IdClase,
+                                idTalla = item.IdTalla,
+                            }
+                        ).ToList();
+                    return lista;
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        private List<SombreroCONSD> GetListaSombrerosDetallada()
+        {
+            try
+            {
+                using (CHAVEZ_HATSContext db = new CHAVEZ_HATSContext())
+                {
+                    List<SombreroCONSD> lista = (
+                            from item in db.Sombreros
+                            join talla in db.Tallas
+                            on item.IdTalla equals talla.IdTalla
+                            join tam in db.TamanoTallas
+                            on item.IdTalla equals tam.IdTalla
+                            join modelo in db.Modelos
+                            on item.IdModelo equals modelo.IdModelo
+                            join clase in db.Clases
+                            on item.IdClase equals clase.IdClase
+                            join material in db.Materials
+                            on item.IdMaterial equals material.IdMaterial
+                            join toquilla in db.Toquillas
+                            on material.IdToquilla equals toquilla.IdToquilla
+                            join forro in db.Forros
+                            on material.IdForro equals forro.IdForro
+                            join barbiquejo in db.Barbiquejos
+                            on material.IdBarbiquejo equals barbiquejo.IdBarbiquejo
+                            join tafiretes in db.Tafiletes
+                            on material.IdTafiletes equals tafiretes.IdTafiletes
+                            where item.Hab == true
+                            select new SombreroCONSD
+                            {
+                                id = item.IdSombrero,
+                                nomModelo = modelo.NomModelo,
+                                nomClase = clase.NomClase,
+                                toquilla = toquilla.NomToquilla,
+                                forro = forro.NomForro,
+                                barbiquejo = barbiquejo.NomBarbiquejo,
+                                tafiretes = tafiretes.NomTafiletes,
+                                ojitos = material.Ojillos ? "Sí" : "No",
+                                resorte = material.Resorte ? "Sí" : "No",
+                                nomTalla = talla.NomTalla,
+                                tamTalla = tam.TamTalla,
+                                medFalda = (float)item.MedFalda,
+                                precio = (float)item.Precio,
+                                stock = (int)item.Stock,
+                                personalizado = (bool)item.Personalizado ? "Sí" : "No",
+                                idModelo = item.IdModelo,
+                                idClase = item.IdClase,
+                                idTalla = item.IdTalla,
+                            }
+                        ).ToList();
+                    return lista;
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        private SombreroCONSD GetSombreroDetallado(int id)
+        {
+            List<SombreroCONSD> lista = GetListaSombrerosDetallada();
+
+            if (lista == null) return null;
+
+            return lista.Find(e => e.id == id);
+        }
+    }
+}
