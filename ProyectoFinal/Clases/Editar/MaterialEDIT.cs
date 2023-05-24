eb017a43bcc184df7c7019497d184b7da5744f3e
Juanjo098 <89929790+Juanjo098@users.noreply.github.com>
2023-05-23 22:38:42 -0600

    Materiales

---
+++ ProyectoFinal/Clases/Editar/MaterialEDIT.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoFinal.Clases.Editar
+{
+    public class MaterialEDIT
+    {
+        [Display(Name ="ID")]
+        [Range(1, int.MaxValue, ErrorMessage ="El ID no es válido")]
+        public int id { get; set; }
+        [Display(Name ="Toquilla")]
+        [Range(1, int.MaxValue, ErrorMessage ="El ID no es válido")]
+        public int idToquilla { get; set; }
+        [Display(Name ="Forro")]
+        [Range(1, int.MaxValue, ErrorMessage ="El ID no es válido")]
+        public int idForro { get; set; }
+        [Display(Name ="Barbiquejo")]
+        [Range(1, int.MaxValue, ErrorMessage ="El ID no es válido")]
+        public int idBarbiquejo { get; set; }
+        [Display(Name ="Tafiretes")]
+        [Range(1, int.MaxValue, ErrorMessage ="El ID no es válido")]
+        public int idTafirete { get; set; }
+        [Display(Name ="Ojillos")]
+        public bool ojillos { get; set; }
+        [Display(Name ="Resorte")]
+        public bool resorte { get; set; }
+    }
+}
---
+++ ProyectoFinal/Clases/Insertar/MaterialINST.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoFinal.Clases.Insertar
+{
+    public class MaterialINST
+    {
+        [Display(Name ="Toquilla")]
+        [Range(1, int.MaxValue, ErrorMessage = "Debe elegir una opción que no sea la predeterminada")]
+        public int idToquilla { get; set; }
+        [Display(Name ="Forro")]
+        [Range(1, int.MaxValue, ErrorMessage = "Debe elegir una opción que no sea la predeterminada")]
+        public int idForro { get; set; }
+        [Display(Name ="Barbiquejo")]
+        [Range(1, int.MaxValue, ErrorMessage = "Debe elegir una opción que no sea la predeterminada")]
+        public int idBarbiquejo { get; set; }
+        [Display(Name ="Tafirete")]
+        [Range(1, int.MaxValue, ErrorMessage = "Debe elegir una opción que no sea la predeterminada")]
+        public int idTafirete { get; set; }
+        [Display(Name ="Ojillos")]
+        public bool ojillos { get; set; }
+        [Display(Name ="Resorte")]
+        public bool resorte { get; set; }
+    }
+}
