a402e8be0dad6078e64c7ce1a9568ff86150011b
Juanjo098 <89929790+Juanjo098@users.noreply.github.com>
2023-05-28 22:52:27 -0600

    Ventas por fecha

---
+++ ProyectoFinal/Clases/Consultas/VentaCONS.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoFinal.Clases.Consultas
+{
+    public class VentaCONS
+    {
+        [Display(Name ="ID")]
+        public int id { get; set; }
+        [Display(Name ="Fecha")]
+        public string fecha { get; set; }
+        [Display(Name ="Total")]
+        public float total { get; set; }
+        [ScaffoldColumn(false)]
+        public DateTime dateTime { get; set; }
+    }
+}
---
+++ ProyectoFinal/Controllers/VentaController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoFinal.Clases.Consultas;
+using ProyectoFinal.Models;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ProyectoFinal.Controllers
+{
+    public class VentaController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<VentaCONS> lista = getListaVenta();
+
+            if (lista == null) return RedirectToAction("Error", "Home");
+
+            return View(lista);
+        }
+
+        [HttpPost]
+        public IActionResult Index(DateTime fechaInicio, DateTime fechaFinal) {
+            if (fechaInicio == DateTime.MinValue || fechaFinal == DateTime.MinValue) return View();
+            if (DateTime.Compare(fechaInicio, fechaFinal) > 0 || DateTime.Compare(fechaInicio, fechaFinal) == 0) return View();
+
+            ViewBag.fechaInicio = fechaInicio;
+            ViewBag.fechaFinal = fechaFinal;
+
+            List<VentaCONS> lista = getListaVenta();
+
+            if (lista == null) return RedirectToAction("Error", "Home");
+
+            lista = lista.FindAll(e => DateTime.Compare(e.dateTime, fechaInicio) > 0 &&
+                DateTime.Compare(e.dateTime, fechaFinal) < 0);
+
+            return View(lista);
+        }
+
+        private List<VentaCONS> getListaVenta()
+        {
+            try
+            {
+                using (CHAVEZ_HATSContext db = new CHAVEZ_HATSContext())
+                {
+                    List<VentaCONS> lista = (
+                            from item in db.Venta
+                            where item.Hab == true
+                            select new VentaCONS
+                            {
+                                id = item.IdVenta,
+                                fecha = item.FchVenta.ToString(),
+                                total = (float)item.TotalVenta,
+                                dateTime = (DateTime)item.FchVenta
+                            }
+                        ).ToList();
+                    return (lista);
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
