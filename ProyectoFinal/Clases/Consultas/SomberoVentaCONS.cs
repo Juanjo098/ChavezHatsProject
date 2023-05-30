93cc0047fab1686ea48638017caa18d7b56bbef5
Juanjo098 <89929790+Juanjo098@users.noreply.github.com>
2023-05-29 22:56:08 -0600

    Reporte de ventas

---
+++ ProyectoFinal/Clases/Consultas/SomberoVentaCONS.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoFinal.Clases.Consultas
+{
+    public class SomberoVentaCONS
+    {
+        [Display(Name ="ID")]
+        public int id { get; set; }
+        [Display(Name ="Modelo")]
+        public string modelo { get; set; }
+        [Display(Name ="Clase")]
+        public string clase { get; set; }
+        [Display(Name ="Precio")]
+        public float precio { get; set; }
+        [Display(Name ="Unidades")]
+        public int unidades { get; set; }
+        [Display(Name ="Subtotal")]
+        public float subTotal { get; set; }
+    }
+}
